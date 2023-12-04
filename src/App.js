94e253c3edecaac4fdad9e98be64a3e96f53c92d
Akhmed <akhmedturan@gmail.com>
2023-12-04 15:10:11 +0600

    up-date

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import { Counter } from "./components/Counter";
+import { useState } from 'react';
+import ProductPage from './components/ProductPage'
 
-function App() {
-  return (
-    <div className="App">
-      <Counter />
-    </div>
-  );
+export default function App() {
+    const [isDark, setIsDark] = useState(false);
+    return (
+        <>
+            <label>
+                <input
+                    type="checkbox"
+                    checked={isDark}
+                    onChange={(e) =>
+                        setIsDark(e.target.checked)
+                    }
+                />
+                Dark mode
+            </label>
+            <hr />
+            <ProductPage
+                referrerId="wizard_of_oz"
+                productId={123}
+                theme={isDark ? 'dark' : 'light'}
+            />
+        </>
+    );
 }
-
-export default App;
--- src/components/CounterValue.jsx
+++
@@ -1,9 +0,0 @@
-
-import React, { useMemo } from 'react'; 
-
-export const CounterValue = ({value}) => {
-    const getResult = useMemo(() => {
-        return value === 5 ? 'Это 5 :)' : 'Это не 5 :('
-    }, [value])
-    return <h2>{getResult}</h2>
-}
